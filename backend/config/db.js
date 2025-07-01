cddf90214ac5cb6911295640b067585e9df64299
Swastik Nanda <swastik2004nanda@gmail.com>
2025-07-01 10:22:50 +0530

    Updated .gitignore and project files

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://raisan:raisan123@cluster0.dclozxw.mongodb.net/wallet-app")
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log(`MongoDB connected: ${conn.connection.host}`.bgGreen.black.bold.underline)
+    console.log(
+      `MongoDB connected: ${conn.connection.host}`.bgGreen.black.bold.underline
+    );
   } catch (err) {
-    console.log(`Error: ${err.message}`.bgRed.bold.underline)
-    process.exit(1)
+    console.log(`Error: ${err.message}`.bgRed.bold.underline);
+    process.exit(1);
   }
-}
+};
 
-module.exports = connectDB
+module.exports = connectDB;
